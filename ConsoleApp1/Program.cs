e72509febf0ddd67d269023fafe03345da8bc40f
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-19 10:58:01 +0100

    Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercises
+{
+    class TypeConversion
+    {
+        static void Main(string[] args)
+        {
+            double x = 15.757;
+            int y = (int)x;
+
+            Console.WriteLine(y);
+        }
+    }
+}
